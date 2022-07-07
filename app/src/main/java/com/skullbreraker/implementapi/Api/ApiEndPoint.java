088331abf22afc633814d519a98bc60da48e5b6c
Tafriyadi27 <tafriyadi273@gmail.com>
2022-07-07 18:29:04 +0700

    - remove unused imports
    - add some code in MainActivity.java:
    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
            getSupportActionBar().setTitle("Daftar Game Gratis");

--- app/src/main/java/com/skullbreraker/implementapi/Api/ApiEndPoint.java
+++ app/src/main/java/com/skullbreraker/implementapi/Api/ApiEndPoint.java
@@ -4,10 +4,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.Headers;
-import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public class ApiEndPoint {
     private static final String BASE_URL = "https://free-to-play-games-database.p.rapidapi.com/api/";
--- app/src/main/java/com/skullbreraker/implementapi/MainActivity.java
+++ app/src/main/java/com/skullbreraker/implementapi/MainActivity.java
@@ -1,7 +1,6 @@
 package com.skullbreraker.implementapi;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.os.BuildCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,17 +8,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.google.gson.internal.GsonBuildConfig;
 import com.skullbreraker.implementapi.Adapter.FreeGameAdapter;
 import com.skullbreraker.implementapi.Api.ApiEndPoint;
 import com.skullbreraker.implementapi.Api.ApiService;
 import com.skullbreraker.implementapi.Model.FreeGameModel;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Spinner List = findViewById(R.id.spinner);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Daftar Game Gratis");
+
         viewAdapter = new FreeGameAdapter(this);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView = findViewById(R.id.recyclerFreegame);
